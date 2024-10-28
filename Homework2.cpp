b8b94b57494d35657a4ca8cc6a6b86bb91f1c201
Peerapat-A <6421061002@cdti.ac.th>
2024-10-28 17:01:10 +0700

    update

---
+++ Homework2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+main(void){
+	float height, weight, bmi
+	printf("Enter you height");
+	scanf("%f", &height)
+	printf("Enter you weight");
+	scanf("%f ")
+}
--- Lab1-2.cpp
+++ Lab1-2.cpp
@@ -5,9 +5,9 @@ main (){
 	char gender= 'M';
 	char name[30]="Patarakarn Homkhajorn";
 	float gpa = 3.95;
-	printf("AGE = %d\n", age);
-	printf("SEX = %c\n", gender);
-	printf("My name is %s \n", name);
-	printf("%s \t %.3f \n", name, PI);
-	printf("GPA = %.2f", gpa);
+//	printf("AGE = %d\n", age);
+//	printf("SEX = %c\n", gender);
+//	printf("My name is %s \n", name);
+//	printf("%s \t %.3f \n", name, PI);
+//	printf("GPA = %.2f", gpa);
 }
