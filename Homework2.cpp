8c3fbd10a1b7c4f9e0414e9cc4032e8668f9ed80
Peerapat-A <6421061002@cdti.ac.th>
2024-10-30 08:54:57 +0700

    update

--- Homework2.cpp
+++ Homework2.cpp
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-main(void){
-	float height, weight, bmi
-	printf("Enter you height");
-	scanf("%f", &height)
-	printf("Enter you weight");
-	scanf("%f ")
+main(void) {
+    float height, weight, BMI;
+
+    printf("Enter your height in meters: ");
+    scanf("%f", &height);
+
+    printf("Enter your weight in kilograms: ");
+    scanf("%f", &weight);
+
+    BMI = weight / (height * height);
+
+    printf("Your BMI is %.3f\n", BMI);
 }
